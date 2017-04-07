7f53a559317771f1df0c7b54b6484fd5e02ceba1
fsarachu <fsarachu@gmail.com>
2017-04-07 04:33:01 -0300

    Add Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+
+            },
+            decrement() {
+
+            }
+        }
+    }
+</script>
